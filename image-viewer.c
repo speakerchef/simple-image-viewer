ebe7b7b9682fd89793ee37783d681c573ba3226e
Sohan Nair <sohannair@chromebook-pro-max.local>
2026-02-28 19:39:23 -0700

    feat: Binary chunkloading

--- image-viewer.c
+++ image-viewer.c
@@ -1,59 +1,87 @@
 #include <SDL3/SDL.h>
+#include <stddef.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
 
-  int x = 900;
-  int y = 600;
-  const SDL_FColor color = {128.0, 0.0, 128.0, 255.0};
-  const SDL_FRect rect = {(float)x / 2, (float)y / 2, 90.0, 60.0};
+    if (argc != 2) {
+        printf("Error: Invalid arguments; expected file-path "
+               "'path/to/image\n");
 
-  const char *ALPHABETS[] = {"A", "B", "C", "D", "E", "F", "G", "H", "I",
-                             "J", "K", "L", "M", "N", "O", "P", "Q", "R",
-                             "S", "T", "U", "V", "W", "X", "Y", "Z"};
-
-  SDL_Window *window = SDL_CreateWindow("Simple Image Viewer", x, y, 0);
-  SDL_CreateRenderer(window, NULL);
-
-  if (window) {
-
-    SDL_Event event;
-    while (true) {
-      while (SDL_PollEvent(&event)) {
-
-        switch (event.type) {
-
-        case SDL_EVENT_WINDOW_CLOSE_REQUESTED:
-          return 0;
-        case SDL_EVENT_KEY_DOWN: {
-          if (30 <= event.key.scancode && event.key.scancode < 39) {
-            int key = event.key.scancode - 29;
-            printf("%d\n", key);
-          } else if (event.key.scancode == 39) {
-            printf("%d\n", 0);
-          } else if (event.key.scancode == 20) {
-            return 0;
-          }
-
-          int length = sizeof(ALPHABETS) / sizeof(char *);
-          for (int i = 0; i < length; i++) {
-            if (event.key.scancode == i + 4)
-              printf("%c\n", *ALPHABETS[i]);
-          }
-        }
-        }
-      }
-
-      // rendering pipeline
-
-      SDL_Renderer *renderer = SDL_GetRenderer(window);
-      SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
-      // SDL_RenderClear(renderer);
-      SDL_RenderRect(renderer, &rect);
-      SDL_RenderPresent(renderer);
-      // TODO: Explore render destructor and other safety/ semantic practies
-      // defined in docs!!
+        return 0;
     }
-  }
-  return 0;
+
+    char *path = argv[1];
+    char buffer[20000] = {0};
+    FILE *file = fopen(path, "r");
+
+    // printf("%ld", fread(&buffer, sizeof(char), 2, file));
+    uint8_t chunk_size = 4;
+    int counter = 0;
+    while (fread(&buffer[counter], sizeof(char), chunk_size, file) != 0) {
+        printf("%s\n", buffer);
+        if (!memcmp(buffer, "IHDR", chunk_size)) {
+            printf("IHDR SEEN");
+        }
+    }
+
+    fclose(file);
+
+    SDL_Window *window = SDL_CreateWindow("Simple Image Viewer", 900, 600, 0);
+    SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED,
+                          SDL_WINDOWPOS_CENTERED);
+    SDL_CreateRenderer(window, NULL);
+
+    int x;
+    int y;
+
+    const SDL_FColor color = {0, 255, 0, 0};
+    const SDL_FRect rect = {(float)x / 2, y, 90, 60};
+
+    // if (window) {
+    //
+    //     SDL_Event event;
+    //     bool isRunning = true;
+    //
+    //     // App loop
+    //     while (isRunning) {
+    //         // printf("Is running: %d\n", isRunning);
+    //
+    //         // Event handling
+    //         while (SDL_PollEvent(&event)) {
+    //
+    //             switch (event.type) {
+    //             case SDL_EVENT_WINDOW_CLOSE_REQUESTED:
+    //                 isRunning = false;
+    //                 break;
+    //             case SDL_EVENT_KEY_DOWN:
+    //                 isRunning = !(event.key.scancode == 20);
+    //                 break;
+    //             }
+    //         }
+    //
+    //         // Rendering pipeline
+    //         SDL_Renderer *renderer = SDL_GetRenderer(window);
+    //
+    //         SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b,
+    //                                color.a);
+    //         SDL_RenderClear(renderer);
+    //         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    //         //
+    //         // Do pixel rendering here.
+    //         //
+    //         SDL_RenderPresent(renderer); // Display
+    //
+    //         // TODO: Explore render destructor and other safety/ semantic
+    //         // practies defined in docs!!
+    //
+    //         // isRunning = false;
+    //     }
+    //
+    //     return 0;
+    // }
+    //
+    // printf("Error opening window.\n");
 }
