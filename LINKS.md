fc80a35544c3e0abce69e8287610862ae7614d28
Sohan Nair <sohannair@chromebook-pro-max.local>
2026-03-01 02:17:29 -0700

    feat: png header data extractio
    took way too much work

---
+++ LINKS.md
@@ -0,0 +1,2 @@
+# https://iter.ca/post/png/
+# https://www.w3.org/TR/PNG-Structure.html
--- image-viewer.c
+++ image-viewer.c
@@ -1,8 +1,24 @@
 #include <SDL3/SDL.h>
+#include <limits.h>
+#include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+typedef struct PNG_Header {
+    uint64_t width;
+    uint64_t height;
+    uint64_t bit_depth;
+    uint64_t color_space;
+    uint64_t compress_method;
+    uint64_t filter_method;
+    uint64_t interlacing;
+} PNG_Header;
+
+static PNG_Header header_params;
 
 int main(int argc, char **argv) {
 
@@ -13,32 +29,99 @@ int main(int argc, char **argv) {
         return 0;
     }
 
-    char *path = argv[1];
-    char buffer[20000] = {0};
-    FILE *file = fopen(path, "r");
+    const char *path = argv[1];
+    FILE *file = fopen(path, "rb");
 
-    // printf("%ld", fread(&buffer, sizeof(char), 2, file));
-    uint8_t chunk_size = 4;
-    int counter = 0;
-    while (fread(&buffer[counter], sizeof(char), chunk_size, file) != 0) {
-        printf("%s\n", buffer);
-        if (!memcmp(buffer, "IHDR", chunk_size)) {
-            printf("IHDR SEEN");
-        }
+    if (!file) {
+        fprintf(stderr, "Could not open file.\n");
+        return 1;
     }
 
+    // Get file length
+    fseek(file, 0, SEEK_END);
+    size_t file_len = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    // Get PNG file identifier (first 8 bytes of the file)
+    char identifier[8] = {0}; 
+    fread(identifier, 1, 8, file);
+    unsigned char *byteid = (unsigned char*)identifier;
+    // for (size_t i = 0; i < 8; i++) {
+    //     printf("%02x", byteid[i]);
+    // }
+    // printf("\n");
+
+    unsigned char __png_id[8] = {0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a}; // PNG Spec ID
+    bool is_PNG = !(memcmp(byteid, __png_id, 8));
+    // printf("%d\n", is_PNG);
+
+    fseek(file, 8, SEEK_SET); // Skip past ID to data chunks
+
+    while(1) {
+        unsigned long chunk_sz = 0;
+        char chunk_type[4]; 
+
+        if (fread(&chunk_sz, 1, 4, file) == 0) break;
+        chunk_sz = ntohl(chunk_sz); //Big to little endian
+        printf("Size of chunk is: %ld, ", chunk_sz);
+
+        if (fread(chunk_type, 1, 4, file) == 0) break;
+        printf("Chunk type is: ");
+        for (size_t i = 0; i < 4; i++) {
+            printf("%c", chunk_type[i]);
+        }
+        printf("\n");
+
+        // Extract image size
+        if (!memcmp(chunk_type, "IHDR", 4)) {
+            /*header is 13 bytes long, stored in big endian
+             *00 00 00 00 | 00 00 00 00 | 00 | 00 | 00 | 00 | 00
+             *     w             h        BD   CS   CM   FM   IL
+             *
+             * */ 
+
+            uint64_t __width;
+            uint64_t __height;
+            unsigned char __bit_depth;
+            unsigned char __color_space;
+            unsigned char __compress_method;
+            unsigned char __filter_method;
+            unsigned char __interlacing;
+
+            fread(&__width, 1, 4, file);
+            fread(&__height, 1, 4, file);
+            fread(&__bit_depth, 1, 1, file);
+            fread(&__color_space, 1, 1, file);
+            fread(&__compress_method, 1, 1, file);
+            fread(&__filter_method, 1, 1, file);
+            fread(&__interlacing, 1, 1, file);
+
+            header_params.width = ntohl(__width);
+            header_params.height = ntohl(__height);
+            header_params.bit_depth = (uint64_t)__bit_depth;
+            header_params.color_space = (uint64_t)__color_space;
+            header_params.compress_method = (uint64_t)__compress_method;
+            header_params.filter_method = (uint64_t)__filter_method;
+            header_params.interlacing = (uint64_t)__interlacing;
+
+            // printf("%llu %llu %llu %llu %llu %llu %llu\n", width, height, bit_depth, color_space, compress_method, filter_method, interlacing);
+
+            fseek(file, -chunk_sz, SEEK_CUR);
+        }
+
+        fseek(file, chunk_sz + 4, SEEK_CUR);
+    }
+      
+    
     fclose(file);
 
-    SDL_Window *window = SDL_CreateWindow("Simple Image Viewer", 900, 600, 0);
+    uint8_t x = header_params.width;
+    uint8_t y = header_params.height;
+
+    SDL_Window *window = SDL_CreateWindow("Simple Image Viewer", x, y, 0);
     SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED,
                           SDL_WINDOWPOS_CENTERED);
     SDL_CreateRenderer(window, NULL);
-
-    int x;
-    int y;
-
-    const SDL_FColor color = {0, 255, 0, 0};
-    const SDL_FRect rect = {(float)x / 2, y, 90, 60};
 
     // if (window) {
     //
